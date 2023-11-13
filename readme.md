cbd24739d924ddf108f93c28e2136fd79f692deb
Taner Tunçer <tankotun@gmail.com>
2023-11-13 16:52:52 +0300

    files

---
+++ readme.md
@@ -0,0 +1,63 @@
+Kimlik bilgilerini, T.C. Nüfus ve Vatandaşlık İşleri Genel Müdürlüğünden doğrular.
+
+##### Önemli Noktalar
+- Doğum Yılı: `birthdate` ile tarih **veya** `birthyear` ile yıl değeri gönderilir.
+- İsim: `name` ile tam isim **veya** `firstname` ve `lastname` ile isim değerleri gönderilir.
+
+##### Örnek
+```js
+const isTurkish = require("isturkish")
+
+await isTurkish({
+	name: "Zeki Müren",
+	birthdate: "1931-12-06",
+	identity: "00000000000",
+}).then(result => {
+
+	// Türk Vatandaşı ise:
+	console.log(result.code) // "turkish" (string)
+	console.log(result.turkish) // true (boolean)
+	console.log(result.person) // {firstname,lastname,birthyear,identity} (object)
+	console.log(result.message) // "Person is Turkish." (string)
+	
+	// Türk Vatandaşı değil ise:
+	console.log(result.code) // "notTurkish" (string)
+	console.log(result.turkish) // false (boolean)
+	console.log(result.person) // {firstname,lastname,birthyear,identity} (object)
+	console.log(result.message) // "Person is not Turkish." (string)
+
+}).catch(error => {
+
+	// Hata oluşur ise:
+	console.log(result.code) // string
+	console.log(result.message) // string
+	
+})
+
+```
+
+##### Parametre Örnekleri
+```js
+// Örnek 1
+await isTurkish({
+	name: "Zeki Müren",
+	birthdate: "1990-10-05",
+	identity: "00000000000",
+})
+
+// Örnek 2
+await isTurkish({
+	firstname: "Zeki",
+	lastname: "Müren",
+	birthyear: "1970",
+	identity: "00000000000",
+})
+
+// Örnek 3
+await isTurkish({
+	firstname: "Falih Rıfkı",
+	lastname: "Atay",
+	birthyear: "1975-10-05",
+	identity: "00000000000",
+})
+```
---
+++ src/isTurkish.js
@@ -0,0 +1,71 @@
+const soap = require("soap")
+
+/**
+ * 
+ * isturkish
+ * Kimlik bilgilerini, T.C. Nüfus ve Vatandaşlık İşleri Genel Müdürlüğünden doğrular.
+ * 
+ * @author Taner Tunçer
+ * 
+ * @async
+ * @function
+ * 
+ * @param {Object} params
+ * @param {string} [params.firstname]
+ * @param {string} [params.lastname]
+ * @param {string} [params.name]
+ * @param {string} params.identity
+ * @param {(date|string)} [params.birthdate]
+ * @param {(number|string)} [params.birthyear]
+ * 
+ * @returns {Promise<{
+ *  code: string,
+ *  turkish: boolean,
+ *  person: { firstname: string, lastname: string, birthyear: string, identity: string },
+ *  message: string
+ * }>}
+ * 
+*/
+async function isTurkish ({ firstname, lastname, name, identity, birthdate, birthyear }) {
+ return new Promise (async (resolve, reject) => {
+   try {
+
+     // Params
+     if(!(name || (firstname && lastname)) || !identity || !(birthdate || birthyear)) {
+       return reject({ code: "params", message: "Params are missing or incorrect!" })
+     }
+
+     // Name
+     if(name) {
+       name = name?.split(' ').filter(f => f !== "")
+       if(name.length <= 1) return reject({ code: "name", message: "Name is incorrect!" })
+     }
+
+     // Values
+     firstname = String(firstname || name.slice(0, name.length - 1).join(' ')).trim().toLocaleUpperCase("tr-TR")
+     lastname = String(lastname || name.slice(-1)).trim().toLocaleUpperCase("tr-TR")
+     birthyear = birthyear || (new Date(birthdate)).getFullYear()
+     identity = identity
+
+     // Request
+     soap.createClient('https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx?WSDL', {}, async (_, client) => {
+       await client.TCKimlikNoDogrulaAsync({
+         Ad: firstname,
+         Soyad: lastname,
+         DogumYili: birthyear,
+         TCKimlikNo: identity,
+       }, (__, result) => {
+        let person = { firstname, lastname, birthyear, identity }
+        if(result.TCKimlikNoDogrulaResult) {
+          resolve({ code: "turkish", turkish: true, person: person, message: "Person is Turkish." })
+        } else {
+          resolve({ code: "notTurkish", turkish: false, person: person, message: "Person is not Turkish!" })
+        }
+       })
+     })
+
+   } catch (error) {
+     reject({ code: "request", message: "Request failed!" })
+   }
+ })
+}
